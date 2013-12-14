c6227f70dbdd930d7fc4cbb976b61e3612e3330f
Alexander Lozovoi <lozovoi604@gmail.com>
2013-12-14 22:13:31 +0400

    Add account_edit()

--- scm.pl
+++ scm.pl
@@ -21,15 +21,34 @@ my $fh_log;
 $fh_log = NewIOFile->new(">> $log_file") or die $! if ($logging_flag);
 
 my $connect_type="console";
-my $username="";
-my $password="";
+my $username="mahadeva";
+my $password="1234567";
 
 my ($exp, $prompt)=&connect_to_switch();
 $exp->send("disable clipaging\n");
 $exp->expect(10,$prompt);
-$exp->send("show account\n");
-my $account_data=($exp->expect(10,$prompt))[3];
-print $account_data;
+
+my %users=(
+    'mahadeva'=>{
+	'password'=>'test',
+	'access_level'=>'admin'
+    },
+    'mahadeva1'=>{
+	'password'=>'test1',
+	'access_level'=>'admin'
+    },
+    'mahadeva2'=>{
+	'password'=>'test2',
+	'access_level'=>'user'
+    },
+    'mahadeva3'=>{
+	'password'=>'test3',
+	'access_level'=>'admin'
+    }
+);
+my $delete_accounts_flag=0;
+
+&account_edit($exp);
 #$exp->send("show config current_config\n");
 #my $config=($exp->expect(undef,
 #				['Next Entry', sub {	my $self = shift;
@@ -38,9 +57,60 @@ print $account_data;
 #				$prompt
 #			))[3];
 
-sub account_data_parse{
-	my $account_data=shift;
-	fore
+sub account_edit{
+	my $exp=shift;
+	$exp->send("show account\n");
+	my $account_data=($exp->expect(10,$prompt))[3];
+	$account_data=~s/[\x0a\x0d]+/\n/g;
+	my @cmd;
+	foreach my $line (split ("\n", $account_data)){
+	    if ($line=~m/(.+?)\s+(Admin|User)\s*$/){
+		my $user=$1;
+		my $access_level=$2;
+		if (exists $users{$user}){
+		    if ($users{$user}->{'access_level'}=~m/$access_level/i){
+			print "User $user with access level $access_level exist\n";
+			delete $users{$user};
+		    }else{
+			print "User $user need change access level from $access_level to $users{$user}->{'access_level'}\n";
+			push (@cmd,['delete',$user]);
+			push (@cmd,['add',$user]);
+		    }
+		}else{
+		    if ($delete_accounts_flag){
+			print "Need delete user $user\n";
+			push (@cmd,['delete',$user]);
+		    }
+		}
+	    }
+	}
+	foreach my $user (keys %users){
+	    print "Need add user $user with access level $users{$user}->{'access_level'}\n";
+	    push (@cmd,['add',$user]);
+	}
+	foreach my $cmd_ref (@cmd){
+	    my $cmd=$cmd_ref->[0];
+	    my $user=$cmd_ref->[1];
+	    if ($cmd eq 'delete'){
+		$exp->send("delete account $user\n");
+		$exp->expect(10,$prompt);
+	    }elsif($cmd eq  'add'){
+		my $access_level=$users{$user}->{'access_level'};
+		my $password=$users{$user}->{'password'};
+		$exp->send("create account $access_level $user\n");
+		$exp->expect(10,
+			[ 'Enter a case-sensitive new password:',
+				sub {	my $self = shift;
+		                	$self->send("$password\n");
+		                        exp_continue; }],
+			[ 'Enter the new password again for confirmation:',
+				sub {	my $self = shift;
+		                        $self->send("$password\n");
+		    	                exp_continue; }],
+		    	$prompt
+		);
+	    }
+	}
 }
 
 sub connect_to_switch{
@@ -161,7 +231,7 @@ sub console_connect {
 	    }
 	    
 	    
-		$exp->log_stdout(0);
+		#$exp->log_stdout(0);
 	    
 	    if ($logging_flag && $debug_flag){
 	        $exp->log_file($fh_log);
